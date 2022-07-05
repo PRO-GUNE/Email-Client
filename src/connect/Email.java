cb037b0c4aca79f295d204d637c00afa6e4d70a4
PRO-GUNE <chathuragunasekera@gmail.com>
2022-07-05 18:29:27 +0530

    Email and Database

--- src/connect/Email.java
+++ src/connect/Email.java
@@ -1,6 +1,27 @@
 package connect;
 
+import recipient.Recipient;
+
 public class Email {
     // This is a singleton object that connects to the Email Service
-    
+    private static Email email;
+    private static final 
+    private String address;
+    private String password;
+
+    private Email(String address, String password){
+        this.address = address;
+        this.password = password;
+    }
+
+    public static Email connectDB(String address, String password){
+        if(email==null){
+            email = new Email(address, password);
+        }
+        return email;
+    }
+
+    public void sendMail(Recipient recipient){
+        // Logic to send the email
+    }
 }
