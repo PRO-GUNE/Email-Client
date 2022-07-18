2bdf8273d5e712f95cc3c24b0e403000ebe839c2
PRO-GUNE <chathuragunasekera@gmail.com>
2022-07-18 19:59:47 +0530

    passed mailing tests

--- test/connect_test/Email_test.java
+++ test/connect_test/Email_test.java
@@ -1,8 +1,13 @@
 package connect_test;
 
+import javax.mail.internet.MimeMessage;
+
+import org.junit.Assert;
 import org.junit.Test;
 
+import connect.Email;
 import connect.MailService;
+import recipient.Recipient;
 import user.User;
 
 public class Email_test {
@@ -10,8 +15,16 @@ public class Email_test {
     
     @Test
     public void sendEmail(){
-        User user = new User("Chathura", "cmggun567@gmail.com", "cmggun123");
+        User user = new User("Chathura", "cmggun567@gmail.com", "uzxlvgylnicxseyn");
         MailService mailService = MailService.connectMailService(user);
-        
+        Assert.assertNotNull(mailService);
+        Assert.assertNotNull(mailService.getSession());
+        Recipient recipient = new Recipient("chathura", "cmg456@gmail.com");
+        Assert.assertNotNull(recipient);
+        Email mail = new Email("wishing you", "Happy birthday", recipient);
+        Assert.assertNotNull(mail);
+
+        // Send the mail
+        mailService.sendMail(mail);
     }
 }
